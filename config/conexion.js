4bcdd9f55148ccda28ed54b8bd9d89fea161f443
diego6967 <caceres.diego@tecnica7.edu.ar>
2025-09-24 20:16:30 -0300

    se agrego la base  de datos y conexion

---
+++ config/conexion.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2/promise';
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tienda',
+    connectionLimit: 5
+});
+
+pool.getConnection()
+    .then(Connection => {
+        console.log('Conexión hecha');
+        Connection.release();
+    })
+    .catch(error => {
+        console.error('Error de conexión');
+    });
+    
+    export default pool;   
